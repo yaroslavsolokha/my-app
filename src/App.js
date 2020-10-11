c09217e3f0b9ddf9fd5f305ba168103877445acf
Yaroslav Solokha <yaroslav.solokha@gmail.com>
2020-10-11 21:32:14 +0200

    using state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    name: "Yaroslav"
+function App() {
+  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("")
+
+  function increment() {
+    setCount(prevCount => prevCount + 1)
   }
 
-
-  render() { 
-    return (
-      <div>
-        Just testing {this.state.name}
-      </div>
-    )
+  function decrement() {
+    setCount(prevCount => prevCount - 1)
   }
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount(prevCount => prevCount + 1)
+    }, 5000)
+  }, [])
+
+  useEffect(() => {
+    if (count === 5) { 
+      setColor("black")
+    } else {
+      setColor("orange")
+    }
+  }, [count])
+
+  return (
+    <div>
+      <h1 style={{color: color}}>{count}</h1>
+      <button onClick={increment}>Increment!</button>
+      <button onClick={decrement}>Decrement!</button>
+    </div>
+  )
 }
 
 export default App;
